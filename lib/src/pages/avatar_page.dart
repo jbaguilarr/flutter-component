e97e29c495e88e94c35855c09ce58fd9edad2f4f
bruno aguilar <josebrunoaguilar@gmail.com>
2019-05-21 23:22:26 -0400

    agregue input y animated container y modifique alert y avatar page

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,26 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text('Avatar Page'),
+          actions: <Widget>[
+            CircleAvatar(
+              backgroundImage: NetworkImage('https://66.media.tumblr.com/63ca79cbcf24cfafd7942db137c8643a/tumblr_pi6oywteYH1s9y3qio2_500.png'),
+              radius: 30.0,
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 10.0),
+              child: CircleAvatar(
+                child: Text('SL'),
+                backgroundColor: Colors.brown,
+              ),
+            )
+          ],
+        ),
+        body: Center(
+          child: FadeInImage(
+            image: NetworkImage('https://images2.minutemediacdn.com/image/upload/c_crop,h_1475,w_2620,x_0,y_272/f_auto,q_auto,w_1100/v1554991977/shape/mentalfloss/71299-gettyimages-627159542.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200)
+          ),
         ),
     );
   }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,7 +1,9 @@
 import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,6 +14,8 @@ return <String,WidgetBuilder>{
         'alert' : (BuildContext context) => AlertPage(),
         'avatar' : (BuildContext context) => AvatarPage(),
         'card' : (BuildContext context) => CardPage(),
+        'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
+        'inputs' : (BuildContext context) => InputPage(),
       };
 
 }
